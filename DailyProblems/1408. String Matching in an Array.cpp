0ce89665f914814d74944efa5db8e9b8edb6f107
Sneha Behera <102979174+snehab03@users.noreply.github.com>
2025-01-08 00:55:59 +0530

    Update and rename 1408. String Matching in an Array to 1408. String Matching in an Array.cpp

--- DailyProblems/1408. String Matching in an Array.cpp
+++ DailyProblems/1408. String Matching in an Array.cpp
@@ -1,7 +1,7 @@
-1408. String Matching in an Array
-Link: https://leetcode.com/problems/string-matching-in-an-array/description/
-Given an array of string words, return all strings in words that is a substring of another word. You can return the answer in any order.
-A substring is a contiguous sequence of characters within a string.
+// 1408. String Matching in an Array
+// Link: https://leetcode.com/problems/string-matching-in-an-array/description/
+// Given an array of string words, return all strings in words that is a substring of another word. You can return the answer in any order.
+// A substring is a contiguous sequence of characters within a string.
 
 class Solution {
 public:
